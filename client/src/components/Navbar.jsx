809d53f6793a06079c69ab21c0d9acad1c82a796
Piyush Gite <96542260+piyushgitelive@users.noreply.github.com>
2022-02-19 00:26:53 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { HiMenuAlt4 } from "react-icons/hi";
 import { AiOutlineClose } from "react-icons/ai";
 
-import logo from "../../images/logo.png";
+import logo1 from "../../images/logo1.png";
 
 // const Navbar = () => {
 
@@ -21,14 +21,25 @@ const Navbar = () => {
   return (
     <nav className="w-full flex md:justify-center justify-between items-center p-4">
       <div className="md:flex-[0.5] flex-initial justify-center items-center">
-        <img src={logo} alt="logo" className="w-32 cursor-pointer" />
+        <img src={logo1} alt="logo" className="w-32 cursor-pointer" />
+        <h1 className="text-white text-base font-bold">
+          Quantum Bridge</h1>
       </div>
       <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
-        {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
+        {/* {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
           <NavBarItem key={item + index} title={item} />
         ))}
-        <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
-          Login
+         <li className="bg-[#292923] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#f54242]">
+        Login
+        </li> */}
+        <li className="bg-[#2e2e26] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#186624]">
+        Buy NFY
+        </li>
+        <li className="bg-[#2e2e26] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#c9832c]">
+        Lock NFY
+        </li>
+        <li className="bg-[#3b3b31] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#f54242]">
+        Migrate NFY
         </li>
       </ul>
       <div className="flex relative">
